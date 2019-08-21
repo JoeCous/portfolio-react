8047eda291ea278ad526b6ef907cf55f827c8e5a
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-21 16:49:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './nevis.png';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-box">
+        <div className="box1"> 
+        <img src={logo} className="nevispic" alt="logo" />
+        <h2>Joe Coucill</h2>
+        <p class="sub">Junior Developer</p>
+        <Aboutme/> 
+        </div>
     </div>
   );
+}
+}
+
+class Aboutme extends React.Component {
+  render() {
+  return(
+    <div className="about-me">
+    <h2>About Me</h2>
+    </div>
+  )
+}
 }
 
 export default App;
