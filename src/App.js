c1677be62582a0d6cc79d572ce2ec4dac55c1e6a
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-23 15:30:05 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import logo from './nevis.png';
 import './App.css';
+import Particles from 'react-particles-js'
 
+const particleOpt = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 class App extends React.Component {
   render() {
-  return (
-    <div className="main-box">
-        <div className="box1"> 
-        <img src={logo} className="nevispic" alt="logo" />
-        <h2>Joe Coucill</h2>
-        <p class="sub">Junior Developer</p>
-        <Aboutme/> 
+    return (
+      <div className="main">
+        <Particles params={particleOpt} className="nono" />
+        <div className="lolo">
+          <img src={logo} className="nevispic" alt="logo" />
+          <h3>Hello, I'm Joe Coucill.</h3> 
+          <p class="sub2">I'm a Junior Software Developer.</p>
+          <button> About me </button>
         </div>
-    </div>
-  );
-}
+          <Aboutme/> 
+      </div>
+    );
+  }
 }
 
 class Aboutme extends React.Component {
   render() {
-  return(
-    <div className="about-me">
-    <h2>About Me</h2>
-    </div>
-  )
+    return (
+      <div className="aboutme">
+        <h3>About Me</h3> 
+        <button> Work </button>
+      </div>
+
+    )
 }
 }
 
+
 export default App;
+
